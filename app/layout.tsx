2e46728e1c99052b4fdf9f327ed48139148ac25c
wirayuda <wirayuda233@gmail.com>
2024-12-08 00:35:35 +0800

    add type import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from 'next/font/google'
+import type { ReactNode } from "react";
+
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { Toaster } from "@/components/ui/sonner";
@@ -53,7 +55,7 @@ const poppins = Poppins({
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
 }>) {
   return (
     <html lang="en">
